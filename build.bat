d788d57b25195ebe60970a00f44c93a9770c6baa
The Goots <mgreen72194@gmail.com>
2025-03-09 19:37:00 -0500

    switch to Odin

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
 @echo off
-call zig build -Dtarget=wasm32-emscripten
-move zig-out\web\* .\
-move /Y main.html index.html
-start cmd /C run_server.bat
+odin build . -out:main -target:js_wasm32
+move main.wasm .
--- run_server.bat
+++ run_server.bat
@@ -1,2 +1,5 @@
 @echo off
-call emrun index.html
+odin build . -out:main -target:js_wasm32
+move main.wasm .
+start cmd /C python3 -m http.server 
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" http://localhost:8000
